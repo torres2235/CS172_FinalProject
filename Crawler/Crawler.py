47848fe54fffc787b713610a6a005f54cfc67bea
torres2235 <43655957+torres2235@users.noreply.github.com>
2021-06-05 13:39:00 -0700

    Add files via upload

--- Crawler/Crawler.py
+++ Crawler/Crawler.py
@@ -1,1 +1,24 @@
+'''
+1.)take urls from seedUrls.txt
+2.)remove from queue/ dequeue
+3.)if already visited that website, go back to the queue for a new one
+4.)if not visited, get the html
+5.)write html to file
+6.)parse html to extra links
+7.)add extracted links to queue
+8.) repeat from (2.)
+
+Needed:
+- Two Lists
+    - queue
+    - visited_urls (for check on (3.) )
+- Library to get HTML
+    - import requests (?)
+    - import urllibs (?)
+        - urllibs.request
+- Scrapper
+    - BeautifulSoup (?)
+        - used to extract links
+'''
+
 
