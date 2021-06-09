939d9ffa7e7ba5107df28a6241be05871a39f8ab
torres2235 <43655957+torres2235@users.noreply.github.com>
2021-06-08 19:27:42 -0700

    More progress on the crawler.
    New issue: how to tell it to stop???
    Also, optimization help!!!

--- Crawler/Crawler.py
+++ Crawler/Crawler.py
@@ -23,24 +23,61 @@ Needed:
 '''
 
 import re
+import time
 import requests # helps get info from webpages
 from bs4 import BeautifulSoup
 
+#---------------------Globals-------------------#
 url_list = []
+queue = []
+visited_urls = []
+#-----------------------------------------------#
+
+#---------------------Open our SeedUrls.txt file-------------------#
 with open('../SeedUrls.txt', 'r') as urls:
     for line in urls:
         line = re.sub('\n',"", line)
         url_list.append(str(line))
 urls.close()
 #print(url_list)
+#-------------------------------------------------------------------#
 
-source_code = requests.get(url_list[0])
-plain_text = source_code.text
-#print(plain_text)
+#-------------------------Stuff-----------------------#
+def crawler(url):
+    try:
+        print(url)
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        #print(plain_text)
+        #print(visited_urls)
 
-soup = BeautifulSoup(plain_text, features = "lxml") # be sure 'pip install lxml' 
-for link in soup.findAll('a'): # 'a' is element for links
-    href = link.get('href')
-    title = link.string
-    print(href)
-    print(title)
+        soup = BeautifulSoup(plain_text, features = "lxml") # be sure 'pip install lxml'
+
+        for link in soup.findAll('a'): # 'a' is element for links
+            href = link.get('href')
+            title = link.string
+            #print(href)
+            #print(title)
+            if href[0] == '/': # checks to see if the href is an extension of our current url
+                current_url = url + href[1:-1]
+                if current_url not in visited_urls:
+                    queue.append(current_url)
+                    visited_urls.append(current_url)
+            else:
+                if href not in visited_urls:
+                    queue.append(href)
+                    visited_urls.append(href)
+    except:
+        print('') # I get a "string index out of range" sometimes and idk why, so this check is here to help
+            
+    #print(queue)
+
+for line in url_list: # start crawling our SeedUrls first
+    crawler(line)
+    time.sleep(0.5) # wait 0.5secs for implicit politeness
+
+for line in queue: # start crawling our queue
+    crawler(line)
+    time.sleep(0.5) # wait 0.5secs for implicit politeness
+
+#print(visited_urls)
