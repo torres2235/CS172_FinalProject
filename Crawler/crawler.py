b4ea005b1076d51bbd92a56b697bd729699abb99
Renzo <rmolivares@renzo-olivares.dev>
2021-06-10 02:22:57 -0700

    Add simhash and break up crawler into different functions

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -28,6 +28,10 @@ import time
 import requests # helps get info from webpages
 from doc import Doc
 from bs4 import BeautifulSoup
+import string
+import hashlib
+import sys
+from queue import LifoQueue
 
 #---------------------Globals-------------------#
 url_list = []
@@ -36,6 +40,8 @@ docs = []
 visited_urls = dict()
 crawled_count = 0
 doc_id = 0
+
+sim_hashes = dict()
 #-----------------------------------------------#
 
 #---------------------Open our SeedUrls.txt file-------------------#
@@ -47,36 +53,46 @@ urls.close()
 #print(url_list)
 #-------------------------------------------------------------------#
 
+def remove_punctuation(token):
+    return token.translate(str.maketrans('', '', string.punctuation))
+
+def remove_stop_words(set_of_tokens, set_of_stop_words):
+    for stop_word in set_of_stop_words:
+        stop_word_regex = re.compile(r'\b{0}\b'.format(stop_word))
+        set_of_tokens = re.sub(stop_word_regex, '', set_of_tokens)
+
+    return set_of_tokens
+
+token_regex = re.compile(r'\w+(?:\.?\w+)*')
+
+# Load stop words
+stop_words_file = open('stopwords.txt', 'r')
+stop_words = []
+for line in stop_words_file:
+    stop_words.extend(line.split())
+stop_words_file.close()
+
 #-------------------------Stuff-----------------------#
 def crawler(url):
     global crawled_count, doc_id
 
     try:
         print(url)
-        source_code = requests.get(url)
-        plain_text = source_code.text
+        raw_website = requests.get(url)
+        raw_html = raw_website.text
 
-        # print(plain_text)
-        #print(visited_urls)
+        parsed_html = BeautifulSoup(raw_html, features = "lxml") # be sure 'pip install lxml'
+        is_content_seen = content_seen(parsed_html.get_text(separator= ' '))
 
-        soup = BeautifulSoup(plain_text, features = "lxml") # be sure 'pip install lxml'
-        # print(soup.get_text(separator=' '))
-        docs.append(Doc('RJP' + str(doc_id), soup.get_text(separator=' ')))
+        if is_content_seen:
+            return
 
-        for link in soup.findAll('a'): # 'a' is element for links
-            href = link.get('href')
-            title = link.string
-            #print(href)
-            #print(title)
-            if href[0] == '/': # checks to see if the href is an extension of our current url
-                current_url = url + href[1:-1]
-                if current_url not in visited_urls:
-                    queue.append(current_url)
-                    visited_urls[current_url] = 1
-            else:
-                if href not in visited_urls:
-                    queue.append(href)
-                    visited_urls[href] = 1
+        full_doc_id = 'RJP' + str(doc_id)
+        docs.append(Doc(full_doc_id, parsed_html.get_text(separator=' ')))
+
+        parsed_links = parsed_html.findAll('a')
+        # robot_url_filter(parsed_links)
+        dup_url_eliminator(parsed_links, url)
         
         crawled_count += 1
         doc_id += 1
@@ -85,12 +101,94 @@ def crawler(url):
             
     #print(queue)
 
+def content_seen(text):
+    global sim_hash
+
+    content_hash = sim_hash(text)
+
+    if content_hash in sim_hashes:
+        return True
+    else:
+        sim_hashes[content_hash] = 1
+        return False
+
+def dup_url_eliminator(links, url):
+    global queue
+
+    for link in links:
+        href = link.get('href')
+
+        if href[0] == '/': # checks to see if the href is an extension of our current url
+            current_url = url + href[1:-1]
+            if current_url not in visited_urls:
+                queue.append(current_url)
+                visited_urls[current_url] = 1
+        else:
+            if href not in visited_urls:
+                queue.append(href)
+                visited_urls[href] = 1
+
+def robot_url_filter(links):
+    #Our target www1.cs.ucr.edu does not have any forbidden links
+    return
+
+def sim_hash(text):
+    global token_regex, sim_hashes
+
+    text = remove_punctuation(text).lower()
+    text = remove_stop_words(text, stop_words)
+    tokens = re.findall(token_regex, text)
+
+    hashes = dict()
+    weights = dict()
+
+    for token in tokens:
+        if token not in weights:
+            weights[token] = 1
+        else:
+            weights[token] += 1
+
+    for token in tokens:
+        hash = (bin(int(hashlib.sha256(bytes(token, encoding='utf-8')).hexdigest(), 16)))[2:]
+
+        if len(hash) < 256:
+            diff = 256 - len(hash)
+            offset = ''
+            for i in range(diff):
+                offset += '0'
+            hash = offset + hash
+        
+        hashes[token] = list(map(int, hash))
+
+    final_hash = []
+
+    current_sum_column = 0
+    current_bit = 0
+
+    while(True):
+        if current_sum_column == 256:
+            break
+
+        for token, value in hashes.items():
+            current_bit += value[current_sum_column] * weights[token] if value[current_sum_column] > 0 else -1 * weights[token]
+
+        current_sum_column += 1
+        final_hash.append(current_bit)
+        current_bit = 0
+
+    for i in range(len(final_hash)):
+        final_hash[i] = 1 if final_hash[i] > 0 else 0
+
+    final_hash = int("".join(map(str,final_hash)))
+
+    return final_hash
+
 for line in url_list: # start crawling our SeedUrls first
     crawler(line)
     time.sleep(0.5) # wait 0.5secs for implicit politeness
 
 for line in queue: # start crawling our queue
-    if crawled_count == 3:
+    if crawled_count == 4:
         break
 
     crawler(line)
